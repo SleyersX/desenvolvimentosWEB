02e0d2b42532475502b3f0008022c11131d91ee2
Walter Moura <walter.araujo.dw@gmail.com>
2021-03-10 19:39:58 -0300

    Update API nodeJS

--- api-petshop/api/rotas/fornecedores/Fornecedor.js
+++ api-petshop/api/rotas/fornecedores/Fornecedor.js
@@ -12,6 +12,7 @@ class Fornecedor {
     }
 
     async criar(){
+        this.validar()
         const resultado = await tabelaFornecedor.inserir({
             empresa: this.empresa,
             email: this.email,
@@ -50,6 +51,21 @@ class Fornecedor {
         }
 
         await TabelaForncedores.atualizar(this.id, dadosParaAtualizar)
+    }
+
+    remover(){
+        return TabelaForncedores.remover(this.id)
+    }
+
+    validar(){
+        const campos = ['empresa', 'email', 'categoria']
+
+        campos.forEach(campo => {
+            const valor = this[campo]
+            if(typeof valor !== 'string' || valor.length === 0){
+                throw new Error(`O campo '${campo}' está inválido`)
+            }
+        })
     }
 }
 
